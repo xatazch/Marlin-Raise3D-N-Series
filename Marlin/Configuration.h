9dbd31df662e98f4892801a226c111864d7ea22b
xatazch <pierre.fransson@outlook.com>
2018-02-11 22:39:43 +0100

    v

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -485,8 +485,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // X and Y offsets must be integers
   #define X_PROBE_OFFSET_FROM_EXTRUDER 44     // Probe on: -left  +right
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 31 // Probe on: -front +behind
-  //#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.59 // -below (always!) ju närmare -0 ju längre från bädden exempel: -2 = nära bädden -1 längre från bädden
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.08 // -below (always!) ju närmare -0 ju längre från bädden exempel: -2 = nära bädden -1 längre från bädden
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER 0.15 // -below (always!) ju närmare -0 ju längre från bädden exempel: -2 = nära bädden -1 längre från bädden
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
 
